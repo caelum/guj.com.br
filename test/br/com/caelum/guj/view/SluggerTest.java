bac9ddeef84008e27f115a93955da6393764e11e
Adriano Almeida <adrianoalmeida7@gmail.com>
2010-12-22 14:48:02 -0200

    testing weird characters when slugging

--- test/br/com/caelum/guj/view/SluggerTest.java
+++ test/br/com/caelum/guj/view/SluggerTest.java
@@ -10,5 +10,7 @@ public class SluggerTest {
 	public void shouldSluggerize() {
 		assertEquals("nome-deve-ficar-assim", new Slugger().sluggerize("nome deve ficar assim"));
 		assertEquals("nome-com-numero-123", new Slugger().sluggerize("nome com numero 123$"));
+		assertEquals("nome-com-numero-123",
+				new Slugger().sluggerize("nome com !ö&#[]{}()\\*/%?$numero 123"));
 	}
 }
