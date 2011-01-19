b94275673c4b810cb49887da03ee2c1c7002775a
Adriano Almeida <adrianoalmeida7@gmail.com>
2011-01-19 13:04:56 -0200

    updating restfulie jar and fixing the newsletter config reader

--- src/br/com/caelum/guj/newsletter/NewsletterConfigs.java
+++ src/br/com/caelum/guj/newsletter/NewsletterConfigs.java
@@ -14,17 +14,23 @@ import br.com.caelum.vraptor.ioc.Component;
 public class NewsletterConfigs {
 
 	private String key;
+	private String listId;
 
 	@PostConstruct
 	public void initialize() throws IOException {
 		Properties properties = new Properties();
-		InputStream stream = NewsletterConfigs.class.getResourceAsStream("/key.properties");
+		InputStream stream = NewsletterConfigs.class.getResourceAsStream("/newsletter_config.properties");
 		properties.load(stream);
 		key = (String) properties.get("newsletter_key");
+		listId = (String) properties.get("list_id");
 		stream.close();
 	}
 
 	public String getAPIKey() {
 		return key;
+	}
+
+	public String getListId() {
+		return listId;
 	}
 }
