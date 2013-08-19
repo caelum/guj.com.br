4c2a7192a3e5b4915684e56311677ff86abfc5f7
leonardowolter <leocwolter@gmail.com>
2013-08-19 18:21:14 -0300

    removendo pasta templates que nao parece fazer nada, só duplicar codigo...(se der pau, só dar revert)

--- templates/default/admin/ajax_test_mail.js
+++
@@ -1,7 +0,0 @@
-$("#mailButton").val("${I18n.getMessage("Config.Form.SmtpTest")}").removeAttr("disabled");
-
-<#if status != "OK">
-	alert("${I18n.getMessage("Config.Form.SmtpTestFail")}: \n ${status}");
-<#else>
-	alert("${I18n.getMessage("Config.Form.SmtpTestSuccess")}");
-</#if>
--- templates/default/js/watch.js
+++
@@ -1,8 +0,0 @@
-function watchTopic(url, message)
-{
-	if (confirm(message)) {
-		document.location = url;
-	}
-}
-
-var watchForum = watchTopic;
